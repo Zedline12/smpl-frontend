b0ca80f266a1d7a13af6779dc37a339e4b2f374a
Zedline12 <bedomohamed307@gmail.com>
2026-03-27 03:31:36 +0200

    update

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -21,7 +21,7 @@ export const SIDEBAR_SECTIONS: SidebarSection[] = [
     title: "Create",
     items: [
       {
-        label: "Image",
+        label: "Generations",
         icon: (
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -36,21 +36,7 @@ export const SIDEBAR_SECTIONS: SidebarSection[] = [
             />
           </svg>
         ),
-        url: "/create/create-image",
-      },
-      {
-        label: "Video",
-        icon: (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="size-6"
-          >
-            <path d="M4.5 4.5a3 3 0 0 0-3 3v9a3 3 0 0 0 3 3h8.25a3 3 0 0 0 3-3v-9a3 3 0 0 0-3-3H4.5ZM19.94 18.75l-2.69-2.69V7.94l2.69-2.69c.944-.945 2.56-.276 2.56 1.06v11.38c0 1.336-1.616 2.005-2.56 1.06Z" />
-          </svg>
-        ),
-        url: "/create/create-image",
+        url: "/create",
       },
     ],
   },
